bed3a1df7c2927497dd41f96212387aa06dbf43b
andrelrocha <andrerocha0911@gmail.com>
2023-05-24 16:02:21 -0300

    feat: adicionada rota para saque, com atenção às regras de negócio

--- src/index.js
+++ src/index.js
@@ -22,6 +22,18 @@ function verifyIfExistsAccountCPF(req, res, next) {
     return next();
 }
 
+function getBalance(statement) {
+    const balance = statement.reduce((acc, operation) => {
+        if (operation.type === 'credit') {
+            return acc + operation.amount
+        } else if (operation.type === 'debit') {
+            return acc - operation.amount
+        }
+    }, 0) 
+
+    return balance;
+}
+
 /* 
 cpf - string - recebe pelo body
 name - string - recebe pelo body
@@ -78,7 +90,25 @@ app.post('/deposit', verifyIfExistsAccountCPF, (req, res) => {
 })
 
 app.post('/withdraw', verifyIfExistsAccountCPF, (req, res) => {
-    
+    const { amount } = req.body;
+    const { customer } = req;
+
+    const balance = getBalance(customer.statement);
+    const insufficientBalance = balance < amount;
+
+    if (insufficientBalance) {
+        return res.status(400).json({error: "Insufficient funds for the operation requested!"})
+    }
+
+    const statementOperation = {
+        amount,
+        created_at: new Date(),
+        type: "debit"
+    }
+
+    customer.statement.push(statementOperation);
+
+    return res.status(201).send();
 })
 
 const port = 3333;
